c0682b74547d65445c17630cf39884ba47f4984f
Aden McCusker <aden.m@null.net>
2023-02-20 15:11:32 +0100

    intermediate push for ec2

--- cog/bot.py
+++ cog/bot.py
@@ -29,9 +29,12 @@ class Bot(commands.Cog):
             await self._bot.load_extension(f'cog.{cog}')
 
         # Sync the command tree to all guilds.
-        await self._bot.tree.sync()
+        synced_cmds = await self._bot.tree.sync()
 
         # Log that the bot is ready.
+        for cmd in synced_cmds:
+            print(f'Successfully synced the \'{cmd.name}\' slash command!')
+
         ready_msg = f'{self._bot.user} is ready!'
         print(ready_msg)
         print('-' * len(ready_msg))
