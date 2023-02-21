013f72152c8c5254fdb0bd6687cc4746947ee79f
Aden McCusker <aden.m@null.net>
2023-02-21 23:47:13 +0100

    Intermediary push for ec2

--- cog/channel/assignment.py
+++ cog/channel/assignment.py
@@ -189,6 +189,9 @@ class ChannelAssignment(commands.Cog):
         # Add all members to the game channel threads.
         for role in roles_to_add:
             await self._add_member_to_threads(role.mention, channel.threads)
+            if len(roles_to_add) > 1:
+                print(f'Want to delete the {role.id} role.')
+                # await role.delete()
 
         # Stop deferring and report that the bot has finished.
         await interaction.followup.send(
