0c7da5efb90172c4ad7fd45cb7c13e218bb744b0
Aden McCusker <aden.m@null.net>
2023-02-25 15:59:45 +0100

    Fixed a bug that stopped members from joining a 'Miscellaneous Games' thread by reacting to it

--- cog/channel/assignment.py
+++ cog/channel/assignment.py
@@ -163,15 +163,11 @@ class ChannelAssignment(commands.Cog):
         if thread.parent.name != MISC_GAMES_CHANNEL_NAME:
             return
 
-        print('passed first')
-
         # If the reaction wasn't added to the first message
         # in the thread, then ignore it.
         first_msg = await get_nth_msg(thread, 1)
         if event.message_id != first_msg.id:
             return
-
-        print('passed second')
 
         # If the member who reacted has already been
         # added to the thread, then ignore it.
@@ -180,11 +176,9 @@ class ChannelAssignment(commands.Cog):
         if reacting_member in thread_members:
             return
 
-        print('passed third')
-
         # Add the member who reacted to the thread.
         await self._add_member_to_threads(
-            reacting_member,
+            reacting_member.mention,
             (thread,),
             misc_games=True
         )
