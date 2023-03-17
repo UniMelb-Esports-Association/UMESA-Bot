dfea2964bc86f6c5dc37840ef3ee0d24edf65f5f
4321louis <45321dumkof@gmail.com>
2023-03-18 02:17:02 +1100

    Initial membership adding code, untested

--- cog/bot.py
+++ cog/bot.py
@@ -8,7 +8,7 @@ from discord.ext import commands
 
 
 # The list of cogs to load.
-_COGS = ('room', 'channel.management', 'channel.assignment', 'misc')
+_COGS = ('room', 'channel.management', 'channel.assignment', 'misc', 'membership')
 
 
 class Bot(commands.Cog):
