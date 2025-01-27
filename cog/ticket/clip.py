914d9e31bc500f405336446e714b760c5e36afc4
Ethan T <54526635+ethan2o@users.noreply.github.com>
2025-01-27 14:36:39 +1100

    remove class methods and attributes

--- cog/ticket/clip.py
+++ cog/ticket/clip.py
@@ -26,32 +26,27 @@ class ClipTicketManagement(TicketManagement):
     MAX_TICKET_ID = 999
     MAX_TICKETS_PER_USER = 3
     TIME_UNTIL_TICKET_STALE = timedelta(weeks=2)
-    BOT = None
-    CATEGORY_ID = None
-    CATEGORY = None
-    ADMIN_ROLE = None
-    used_ticket_ids = []
 
     def __init__(self, bot: commands.Bot) -> None:
 
         super().__init__(bot)
         self._data = TicketData()
         
-        ClipTicketManagement.BOT = bot
-        ClipTicketManagement.CATEGORY_ID = (
+        self._bot = bot
+        self._category_id = (
             self._data.module("clip")["category_id"]
         )
-        ClipTicketManagement.CATEGORY = discord.utils.get(
-            self.bot.guilds[0].categories, id=ClipTicketManagement.CATEGORY_ID
+        self._category = discord.utils.get(
+            self.bot.guilds[0].categories, id=self._category_id
         )
-        ClipTicketManagement.ADMIN_ROLE = self._data.module("clip")["role_id"]
+        self._admin_role = self._data.module("clip")["role_id"]
         # get all used ticket ids
-        ClipTicketManagement.used_ticket_ids = [
+        self._used_ticket_ids = [
             int(channel.name[-3:])
-            for channel in ClipTicketManagement.CATEGORY.channels]
-        
-    @classmethod    
-    def get_next_ticket_id(cls):
+            for channel in self._category.channels]
+
+         
+    def get_next_ticket_id(self):
         """Retrieves the next valid ticket Id
         Finds Id based on the following:
         Get highest ticket number possible, or
@@ -64,13 +59,13 @@ class ClipTicketManagement(TicketManagement):
             Ticket Id
         """
         
-        if not cls.used_ticket_ids:
+        if not self._used_ticket_ids:
             return 1
         
-        ticket_id = cls.used_ticket_ids[-1]
+        ticket_id = self._used_ticket_ids[-1]
         
-        while ticket_id in cls.used_ticket_ids:
-            if ticket_id == cls.used_ticket_ids:
+        while ticket_id in self._used_ticket_ids:
+            if ticket_id == self._used_ticket_ids:
                 ticket_id = 1
             else:
                 ticket_id += 1
@@ -89,7 +84,7 @@ class ClipTicketManagement(TicketManagement):
         
         user_roles = [role.id for role in user.roles]
         
-        if ClipTicketManagement.ADMIN_ROLE in user_roles:
+        if self._admin_role in user_roles:
             return True
         
         return False
@@ -133,7 +128,7 @@ class ClipTicketManagement(TicketManagement):
         embed = self.create_embed(embed_title, embed_text, embed_colour)
         await self.send_embed(interaction.channel, embed)
         view = discord.ui.View(timeout=None)
-        view.add_item(TicketButton(button_label, button_emoji))
+        view.add_item(TicketButton(self, button_label, button_emoji))
         try:
            await self.send_view(interaction.channel, view)
         except:
@@ -143,9 +138,8 @@ class ClipTicketManagement(TicketManagement):
         await interaction.response.send_message(
             "Ticket booth created", ephemeral=True)
     
-    @classmethod
     async def create_ticket(
-        cls, 
+        self, 
         interaction: discord.Interaction,
         ) -> None:
         """Creates a new ticket
@@ -158,45 +152,40 @@ class ClipTicketManagement(TicketManagement):
         
         num_tickets_opened = 0
         member_roles = [role.id for role in interaction.user.roles]
-        instance = ClipTicketManagement(cls.BOT)
         
         # ignore maximum tickets for allowed users (specified in init)
-        if cls.ADMIN_ROLE not in member_roles:
-            for channel in cls.CATEGORY.channels:
+        if self._admin_role not in member_roles:
+            for channel in self._category.channels:
                 if interaction.user in channel.members:
                     num_tickets_opened += 1
 
         # check if user more tickets opened than allowed
-        if num_tickets_opened >= cls.MAX_TICKETS_PER_USER:
+        if num_tickets_opened >= ClipTicketManagement.MAX_TICKETS_PER_USER:
             await interaction.response.send_message(
                 "ERROR: Maximum number of tickets opened", ephemeral=True)
             return
 
         permission = discord.PermissionOverwrite(view_channel=True)
-        ticket_id = cls.get_next_ticket_id()
-        channel = await ClipTicketManagement.create_channel(
-            instance,
+        ticket_id = self.get_next_ticket_id()
+        channel = await self.create_channel(
             f"ticket-{ticket_id:03d}",
-            cls.CATEGORY_ID,
+            self._category_id,
             interaction.user,
             permission
         )
-        cls.used_ticket_ids.append(ticket_id)
+        self._used_ticket_ids.append(ticket_id)
         
-        embeds = ClipTicketManagement.load_embed(
-            instance, "cog/ticket/clip_questions.json"
-            )
+        embeds = self.load_embed("cog/ticket/clip_questions.json")
         
         for embed in embeds:
-            await ClipTicketManagement.send_embed(instance, channel, embed)
+            await self.send_embed(channel, embed)
         await channel.send(f"{interaction.user.mention}")
-        await ClipTicketManagement.send_view(instance, channel, HideButton())
+        await self.send_view(channel, HideButton())
         await interaction.edit_original_response(content="Ticket created")
     
-    @classmethod
     @commands.Cog.listener()
     async def on_guild_channel_delete(
-        cls,
+        self,
         channel: discord.abc.GuildChannel
     ) -> None:
         """Update the list of used ticket ids when a channel is deleted
@@ -204,9 +193,10 @@ class ClipTicketManagement(TicketManagement):
         Args:
             channel: The channel that was deleted
         """
-        
+
         # Only check for channels in this category
-        if channel.category_id != cls.CATEGORY_ID:
+        if channel.category_id != self._category_id:
+            print("Ticket deleted")
             return
     
     @app_commands.command(
@@ -236,7 +226,7 @@ class ClipTicketManagement(TicketManagement):
         
         await interaction.response.defer(thinking=True, ephemeral=True)
         
-        for channel in ClipTicketManagement.CATEGORY.channels:
+        for channel in self._category.channels:
             last_message = channel.history(limit=1)
             date = [message.created_at async for message in last_message][0]
             if (date < stale_date):
@@ -252,7 +242,8 @@ class TicketButton(
     template=r'make_ticket:([0-9]+)'):
     """Dynamic, persistent button to handle ticket creation"""
     
-    def __init__(self, label=None, emoji=None):
+    def __init__(self, ticket_manager, label=None, emoji=None):
+        self._ticket_manager = ticket_manager
         super().__init__(
             discord.ui.Button(
                 label=label,
@@ -266,12 +257,13 @@ class TicketButton(
     async def from_custom_id(cls,
                              interaction: discord.Interaction,
                              item: discord.ui.Button, 
-                             match: re.Match[str], /):
+                             match: re.Match[str]):
         time = int(match.group(1))
-        return cls(time)
+        ticket_manager = interaction.client.ticket_manager_instance
+        return cls(ticket_manager)
 
     async def callback(self, interaction):
-        await ClipTicketManagement.create_ticket(interaction)
+        await self._ticket_manager.create_ticket(interaction)
 
         
 class HideButton(discord.ui.View):
@@ -404,5 +396,7 @@ async def setup(bot: commands.Bot) -> None:
     """
     instance = ClipTicketManagement(bot)
     bot.add_view(HideButton())
+    # store instance of ClipTicketManagement to be used in TicketButton
+    bot.ticket_manager_instance = instance 
     bot.add_dynamic_items(TicketButton)
     await bot.add_cog(instance)
