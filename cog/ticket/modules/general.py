b94057ec07698f8b35381bbbcb0608daf947c58f
Ethan T <54526635+ethan2o@users.noreply.github.com>
2025-02-24 21:35:48 +1100

    add new ticket modules

---
+++ cog/ticket/modules/general.py
@@ -0,0 +1,23 @@
+"""Handles creating tickets for general ticket submission
+"""
+from ..ticket_module import TicketModule
+from discord.ext import commands
+
+TICKET_PREFIX = "general"
+EMBED_PATH = None
+
+class Ticket(TicketModule):
+    def __init__(self, bot: commands.Bot) -> None:
+        super().__init__(bot, TICKET_PREFIX, EMBED_PATH)
+        
+async def setup(bot: commands.Bot) -> None:
+    """A hook for the bot to register the ticket cog.
+    Also stores an instance of itself within the Discord bot instance for
+    future reference
+
+    Args:
+        bot: The bot to add this cog to.
+    """
+    instance = Ticket(bot)
+    bot.instances[TICKET_PREFIX] = instance
+    await bot.add_cog(instance)
