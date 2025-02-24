49e22bc651cf4bd455cf8dfc0ba8716c6981d440
Ethan T <54526635+ethan2o@users.noreply.github.com>
2025-02-24 21:22:06 +1100

    remove TicketButton

--- cog/ticket/interactables.py
+++ cog/ticket/interactables.py
@@ -47,32 +47,3 @@ class HideButton(discord.ui.View):
         user = [message.author async for message in last_message][1]
         if user == interaction.client.user:
             await interaction.channel.delete()
-            
-
-class TicketButton(
-    discord.ui.DynamicItem[discord.ui.Button],
-    template=r'make_ticket:([0-9]+)'):
-    """Dynamic, persistent button to handle ticket creation"""
-    
-    def __init__(self, ticket_manager, label=None, emoji=None):
-        self._ticket_manager = ticket_manager
-        self._ticket_prefix = ticket_manager._ticket_prefix
-        super().__init__(
-            discord.ui.Button(
-                label=label,
-                emoji=emoji,
-                style=discord.ButtonStyle.blurple,
-                custom_id=f"make_ticket:{int(time.time())}"
-            )
-        )
-
-    @classmethod
-    async def from_custom_id(cls,
-                             interaction: discord.Interaction,
-                             item: discord.ui.Button, 
-                             match: re.Match[str]):
-        ticket_manager = interaction.client.instances[cls.ticket_prefix]
-        return cls(ticket_manager)
-
-    async def callback(self, interaction):
-        await self._ticket_manager.create_ticket(interaction)
